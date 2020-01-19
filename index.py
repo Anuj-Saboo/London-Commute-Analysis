51676611a630d9523afc4622d2af77214ee3fc60
saboo.anuj17 <saboo.anuj17@gmail.com>
2020-01-18 16:59:17 -0800

    Update Project

--- index.py
+++ index.py
@@ -1,14 +1,9 @@
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 from dash.dependencies import Input, Output, State
-import dash_bootstrap_components as dbc
-#from app import App, build_graph
 from app import app
-#from Borough import borough
-from apps import twitter,Borough,park,stationusage,accidentmap, twittercloud
-from homepage import Homepage
+from apps import Borough,park,stationusage,accidentmap,boroughload,twittercloud,twitter,accidentfactor,cycleusage,bus_usage
+import homepage
 
 
 def toggle_collapse(n, is_open):
@@ -21,8 +16,7 @@ def set_navitem_class(is_open):
         return "open"
     return ""
 
-
-for i in [1, 2,3]:
+for i in [1, 2,3,4,5]:
     app.callback(
         Output(f"submenu-{i}-collapse", "is_open"),
         [Input(f"submenu-{i}", "n_clicks")],
@@ -35,7 +29,7 @@ for i in [1, 2,3]:
     )(set_navitem_class)
 
 CONTENT_STYLE = {
-    "font-family" : "Georgia"
+    "font-family" : "Arial"
 }
 app.layout = html.Div([
     dcc.Location(id = 'url', refresh = False),
@@ -46,24 +40,31 @@ app.config['suppress_callback_exceptions']=True
 @app.callback(Output('page-content', 'children'),
             [Input('url', 'pathname')])
 def display_page(pathname):
+
     if pathname == '/apps/t_sentiment':
         return twitter.layout
     elif pathname == '/apps/t_cloud':
-        #return twittercloud.layout
-        return Homepage()
+        return twittercloud.layout
     elif pathname == '/apps/a_borough':
         return Borough.layout
+    elif pathname =='/apps/a_accidentfactors':
+        return accidentfactor.layout
     elif pathname =='/apps/a_park':
         return park.layout
+    elif pathname =='/apps/a_cycleusage':
+        return cycleusage.layout
+    elif pathname == '/apps/a_boroughload':
+        return boroughload.layout
     elif pathname =='/apps/a_stationusage':
         return stationusage.layout
     elif pathname =='/apps/a_accidentmap':
         return accidentmap.layout
-
+    elif pathname == '/apps/perf':
+        return bus_usage.layout
     elif pathname == '/home':
-        return Homepage()
+        return homepage.layout
     else:
-        return Homepage()
+        return homepage.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- navbar.py
+++ navbar.py
@@ -12,39 +12,32 @@ SIDEBAR_STYLE = {
     "padding": "2rem 1rem",
     "background-color": "#4c4c57",
     "color": "#f4f5f7",
-    #"color" : "dark",
-    #"dark" : "True"
     "font-family" : "Arial"
 }
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-
 
 submenu_1 = [
     html.Li(
         # use Row and Col components to position the chevrons
         dbc.Row(    #Screen is split in rows and columns. This is the first row with 2 columns
             [
-
-                dbc.Col("Twitter"),
+                dbc.Col(html.I(className="fab fa-twitter"),width="2"),
+                dbc.Col("Twitter",width="6"),
                 dbc.Col(
-                    html.I(className="fas fa-chevron-down mr-3"), width="auto" #-down was -right. mr-X, X is the position
+                    html.I(className="fas fa-chevron-down mr-2") #-down was -right. mr-X, X is the position
                 ),
 
             ],
+            no_gutters=True,
             className="my-1",
         ),
         id="submenu-1",
     ),
-    # we use the Collapse component to hide and reveal the navigation links
+
     dbc.Collapse(
         [
             dbc.NavLink("Sentiment Analysis", href="/apps/t_sentiment"),
             dbc.NavLink("Word Cloud", href="/apps/t_cloud"),
-
         ],
-
         id="submenu-1-collapse",
     ),
 ]
@@ -53,11 +46,13 @@ submenu_2 = [
     html.Li(
         dbc.Row(
             [
-                dbc.Col("Accidents"),
+                dbc.Col(html.I(className="fas fa-car-crash"),width="2"),
+                dbc.Col("Accidents",width="6"),
                 dbc.Col(
-                    html.I(className="fas fa-chevron-down mr-3"), width="auto"
+                    html.I(className="fas fa-chevron-down mr-2"),
                 ),
             ],
+            no_gutters=True,
             className="my-1",
         ),
         id="submenu-2",
@@ -66,6 +61,7 @@ submenu_2 = [
         [
             dbc.NavLink("Borough Report", href="/apps/a_borough"),
             dbc.NavLink("Accident Locations", href="/apps/a_accidentmap"),
+            dbc.NavLink("Accident Factors", href="/apps/a_accidentfactors")
         ],
         id="submenu-2-collapse",
     ),
@@ -73,108 +69,106 @@ submenu_2 = [
 
 submenu_3 = [
     html.Li(
-        dbc.Row(
+        # use Row and Col components to position the chevrons
+        dbc.Row(    #Screen is split in rows and columns. This is the first row with 2 columns
             [
-                dbc.Col("Usage Statistics"),
+                dbc.Col(html.I(className="fas fa-subway"),width="2"),
+                dbc.Col("Tube",width="6"),
                 dbc.Col(
-                    html.I(className="fas fa-chevron-down mr-3"), width="auto"
+                    html.I(className="fas fa-chevron-down mr-2") #-down was -right. mr-X, X is the position
                 ),
+
             ],
+            no_gutters=True,
             className="my-1",
         ),
         id="submenu-3",
     ),
+
     dbc.Collapse(
         [
-            dbc.NavLink("Parking Infrastructure", href="/apps/a_park"),
-            dbc.NavLink("Tube Usage", href="/apps/a_stationusage"),
-
+            dbc.NavLink("Borough Distribution", href="/apps/a_boroughload"),
+            dbc.NavLink("Station Congestion", href="/apps/a_stationusage"),
         ],
         id="submenu-3-collapse",
+    ),
+]
+
+submenu_4 = [
+    html.Li(
+        dbc.Row(
+            [
+                dbc.Col(html.I(className="far fa-chart-bar"),width="2"),
+                dbc.Col("Facilities",width="6"),
+                dbc.Col(
+                    html.I(className="fas fa-chevron-down mr-2"),
+                ),
+            ],
+            no_gutters=True,
+            className="my-1",
+        ),
+        id="submenu-4",
+    ),
+    dbc.Collapse(
+        [
+
+            dbc.NavLink("Cycle Usage", href="/apps/a_cycleusage"),
+            dbc.NavLink("Parking", href="/apps/a_park"),
+
+
+
+        ],
+        id="submenu-4-collapse",
+    ),
+]
+
+submenu_5 = [
+    html.Li(
+        dbc.Row(
+            [
+                dbc.Col(html.I(className="fas fa-bus-alt"),width="2"),
+                dbc.Col("Bus",width="6"),
+                dbc.Col(
+                    html.I(className="fas fa-chevron-down mr-2"),
+                ),
+            ],
+            no_gutters=True,
+            className="my-1",
+        ),
+        id="submenu-5",
+    ),
+    dbc.Collapse(
+        [
+            dbc.NavLink("Performance", href="/apps/perf"),
+        ],
+        id="submenu-5-collapse",
     ),
 ]
 def Navbar():
     navbar = html.Div(
         [
-            html.Img(src='/assets/n9hyiWD.png',
-            style={
-                        'height' : '20%',
-                        'width' : '100%',
-                        #'float' : 'left',
-                        'border-radius': '50%',
-                        'position' : 'relative',
-                        'padding-top' : 0,
-                        'padding-right' : 0
-                    }
-                     )
-                    ,
-            #html.H2("Sidebar", className="display-4"),
-            html.Hr(), #This is a horizontal line
+            html.A(
+                [
+                    html.Img(src='/assets/n9hyiWD.png',
+                             style={
+                                 'height': '20%',
+                                 'width': '100%',
+                                 'border-radius': '50%',
+                                 'position': 'relative',
+                                 'padding-top': 0,
+                                 'padding-right': 0
+                             }
+                             ),
+                ],href='/home'
+            ),
+
+            html.Hr(),
             html.P(
                 "", className="lead"
-            ),  #This is a paragraph
-            dbc.Nav(submenu_1 + submenu_2 + submenu_3, vertical=True),
+            ),
+            dbc.Nav(submenu_2 + submenu_3 + submenu_5 + submenu_4 + submenu_1, vertical=True),
         ],
         style=SIDEBAR_STYLE,
         id="sidebar",
-        #color = "dark",
-        #dark = True
     )
     return navbar
-
-
-
-'''
-
-def Navbar():
-     navbar = dbc.Nav(
-           children=[
-              #dbc.NavItem(dbc.NavLink("Twitter Sentiment Analysis", href="/time-series")),
-               html.A(
-                   # Use row and col to control vertical alignment of logo / brand
-                   dbc.Row(
-                       [
-                           dbc.Col(html.Img(src='https://imgur.com/cLQBCvo.png', height="Auto",width = "150px")),
-                           #dbc.Col(dbc.NavbarBrand("Navbar", className="ml-2")),
-                       ],
-                       align="center",
-                       no_gutters=True,
-                   ),
-                   href="/home",
-               ),
-              dbc.DropdownMenu(
-                 nav=True,
-                 in_navbar=True,
-                 label="Twitter",
-                 children=[
-                    #dbc.DropdownMenuItem("Sentiment Analysis"),
-                    dbc.NavItem(dbc.NavLink("Sentiment Analysis", href="/t_sentiment")),
-                    dbc.NavItem(dbc.NavLink("Word Cloud", href="/t_wordcloud")),
-                    #dbc.DropdownMenuItem("Word Cloud"),
-                    #dbc.DropdownMenuItem(divider=True),
-                    #dbc.DropdownMenuItem("Entry 3"),
-                          ],
-                      ),
-               dbc.DropdownMenu(
-                   nav=True,
-                   in_navbar=True,
-                   label="Accident",
-                   children=[
-                       # dbc.DropdownMenuItem("Sentiment Analysis"),
-                       dbc.NavItem(dbc.NavLink("Borough Report", href="/a_borough")),
-                       #dbc.NavItem(dbc.NavLink("Word Cloud", href="/t_wordcloud")),
-                       # dbc.DropdownMenuItem("Word Cloud"),
-                       # dbc.DropdownMenuItem(divider=True),
-                       # dbc.DropdownMenuItem("Entry 3"),
-                   ],
-               ),
-                    ],
-          #brand="Home",
-          #brand_href="/home",
-          #sticky="top",
-         #vertical=True
-        )
-     #return sidebar
-     return navbar
-
-'''
